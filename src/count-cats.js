d901e1e2a254ea403926435bcde909de2a4a8ad4
antonina220590 <tonya2290@gmail.com>
2023-12-14 07:50:29 +0300

    feat: add solutions for count-cats and carbon-dating tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let catsEars = [];
+  for (let i = 0; i < matrix.length; i++) {
+   for(let j = 0; j < matrix[i].length; j++) {
+    if(matrix[i][j] === '^^') {
+     catsEars.push(matrix[i][j])
+    }
+   }
+  }
+return catsEars.length
 }
 
 module.exports = {
